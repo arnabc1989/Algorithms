bedba6dfa89fe6de895f3851574575a284b63eb7
arnabc1989 <96055249+arnabc1989@users.noreply.github.com>
2024-11-17 00:44:05 +0530

    Binary Tree Check

---
+++ Tree/BST/BSTCheck/Program.cs
@@ -0,0 +1,85 @@
+﻿// C# program to check if a tree is BST 
+// using Morris Traversal
+
+using System;
+
+class Node
+{
+    public int data;
+    public Node left, right;
+    public Node(int value)
+    {
+        data = value;
+        left = right = null;
+    }
+}
+
+class BSTCheck
+{
+    public static bool isBST(Node root)
+    {
+        Node curr = root;
+        Node pre;
+
+        int prevValue = int.MinValue;
+        while (curr != null)
+        {
+            if (curr.left == null)
+            {
+                if (curr.data <= prevValue) { return false; }
+                prevValue = curr.data;
+                curr = curr.right;
+            }
+            else
+            {
+                pre = curr.left;
+                while (pre.right != null && pre.right != curr)
+                {
+                    pre = pre.right;
+                }
+
+                if(pre.right == null)
+                {
+                    pre.right = curr;
+                    curr = pre.left;
+                }
+                else
+                {
+                    pre.right = null;
+                    if (curr.data <= prevValue)
+                    {
+                        return false;
+                    }
+                    prevValue = curr.data;
+                    curr = curr.right;
+                }
+            }
+        }
+        return true;
+    }
+    static void Main(string[] args)
+    {
+        // Create a sample binary tree
+        //      4
+        //    /   \
+        //   2     5
+        //  / \
+        // 1   3
+
+        //Use Case: Spell checkers and autocorrect
+        Node root = new Node(4);
+        root.left = new Node(6);
+        root.right = new Node(5);
+        root.left.left = new Node(1);
+        root.left.right = new Node(5);
+
+        if (isBST(root))
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+    }
+}
